8c635b9f85ccabff7989ba0e27e603f805a27f1a
Sikander Nishad <skaicybertron@gmail.com>
2020-07-27 06:38:26 +0100

    add redirects files

---
+++ build/precache-manifest.279bba9045ddfbf2f2be7e7df4b7db2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1591f18134096877e71caabc750d9db2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f462ea5ab890fc8b2e2",
+    "url": "/static/css/main.2155c280.chunk.css"
+  },
+  {
+    "revision": "a6ac2e83ec7c797a0bbf",
+    "url": "/static/js/2.95616783.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.95616783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f462ea5ab890fc8b2e2",
+    "url": "/static/js/main.6081c38e.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
